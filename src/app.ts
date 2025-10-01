a91acdd96d627235584129be9334c8823509b505
Wilker Martins <dev.wilker@gmail.com>
2025-10-01 14:34:21 -0300

    dfg

--- src/app.ts
+++ src/app.ts
@@ -60,6 +60,7 @@ app.use(express.json());
 app.use(mongoSanitize());
 
 // Rotas da Aplicação
+//roat
 app.use('/api/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/clientes', clienteRoutes);
